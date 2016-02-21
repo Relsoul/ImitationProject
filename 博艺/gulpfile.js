6804c86f6b4f403b5e036ba87edf606df49ef45e
relsoul <admin@emufan.com>
2016-02-21 14:06:39 +0800

    fix gulp include html and change agent dir

--- 博艺/gulpfile.js
+++ 博艺/gulpfile.js
@@ -4,15 +4,17 @@ var p=require("gulp-load-plugins")();
 gulp.task("default",["concat:watch"])
 
 gulp.task("concat:watch",function(){
-    gulp.watch("./*/*.html",function(event){
+    gulp.watch("./**/*.html",function(event){
         concatCompile(event.path)
     })
 })
 
+
 function concatCompile(path){
     //console.log(14,path)
     var file=path.split("\\");
-    var path_file=file[file.length-2];
+    var path_file=file.slice(0,-1);
+    path_file=path_file.join("\\");
     var path_name=file[file.length-1];
     //console.log(17,file)
     if(path_name=="index.html"){
@@ -24,7 +26,7 @@ function concatCompile(path){
             includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g,
         }))
         .pipe(p.rename("index.html"))
-        .pipe(gulp.dest("./"+path_file))
+        .pipe(gulp.dest(path_file))
 }
 
 /*
